f231442ff7ecff6875d91da2f132fe93487ecdf2
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-19 20:52:17 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git init
-git clone https://github.com/eplexity/pulumi-sandbox.git
+git clone https://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:github.com/eplexity/pulumi-sandbox.git
 ls -ltR
 git fetch origin  
 git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
