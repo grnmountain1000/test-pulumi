5e578e3a61ce4f42ee86330f43f8f1812e1d6eab
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-19 22:40:41 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -2,7 +2,7 @@
 aws s3 ls
 echo $GIT_STRING
 git init
-git clone `$GIT_STRING`
+git clone $GIT_STRING
 #git clone https://{$GIT_TOKEN}:github.com/eplexity/pulumi-sandbox.git
 ls -ltR
 ##git fetch origin  
