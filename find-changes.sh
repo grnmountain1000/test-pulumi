b1dbe5d0e079e23b7f0d2cdee600fa4ba8154293
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-19 22:56:38 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -4,7 +4,7 @@ echo $GIT_STRING
 git init
 git clone $GIT_STRING
 #ls -ltR
-#git fetch origin  
+git pull --unshallow 
 #git fetch 
 #git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
 
