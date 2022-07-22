cb373e6a3e9756979fed28d9e802d27a8d0be989
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-22 11:45:07 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -5,12 +5,15 @@ git init
 #git clone  --mirror  $GIT_STRING
 git clone   --depth 2  $GIT_STRING
 
-#ls -ltR
+# move into the repo
+cd pulumi-sandbox
+
+
 #git rev-parse origin/main
 
 #git rev-parse --verify HEAD
 git rev-parse --verify $GIT_TOKEN
 
-#git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
+git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
 
 
