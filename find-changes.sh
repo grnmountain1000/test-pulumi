981148c66567ad6b4c7f30f389d6041d60822367
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-22 10:34:34 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -2,7 +2,7 @@
 aws s3 ls
 echo $GIT_STRING
 git init
-git clone $GIT_STRING
+git clone  --mirror  $GIT_STRING
 #ls -ltR
 #git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
 
