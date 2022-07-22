314174189584c760431cea60f912dd4036813231
grnmountain1000 <105369100+grnmountain1000@users.noreply.github.com>
2022-07-22 12:14:12 -0400

    Update find-changes.sh

--- find-changes.sh
+++ find-changes.sh
@@ -14,8 +14,9 @@ cd test-pulumi
 #git rev-parse origin/main
 
 #git rev-parse --verify HEAD
-git rev-parse --verify $GIT_TOKEN
+#git rev-parse --verify $GIT_TOKEN
 
+echo "The changed files this commit are: "
 git diff-tree --no-commit-id --name-only -r $(git rev-parse --verify HEAD)
 
 
